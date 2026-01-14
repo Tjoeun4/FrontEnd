d89441a6d6c89d0547143c8fcdc0007ea12089a6
xxon <163013769+Eunsseob@users.noreply.github.com>
2026-01-14 17:19:47 +0900

    feat : API 사용 구조 간소화 및 보안 강화

---
+++ nang_guide/lib/features/auth/services/api_service.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import './../models/spring_response_model.dart';
+// import '/services/token_service.dart';
+import '../controllers/auth_controller.dart';
+
+class ApiService {
+  final AuthController _authController = Get.find<AuthController>();
+  // final TokenService _tokenService = Get.find<TokenService>();
+  var count = 0;
+
+  Future<Map<String, dynamic>> getRequest(String endpoint) async {
+    // vra token = await _tokenService.loadToken();
+    // if (token == null) throw Exception("No token found");
+    final url = "우리가 들어갈 API 주소 입니다./$endpoint";
+
+    final springResponse = await http.get(
+      Uri.parse(url),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        // 'accesstoken': token.accessToken
+      },
+    );
+
+    final SpringResponse response = SpringResponse.fromJson(jsonDecode(utf8.decode(springResponse.bodyBytes)));
+
+    // if (response.statusCode == 401) {
+    //   bool refreshed = await _authController.handle401();
+    //   if (refreshed) {
+    //     return getRequest(endpoint); // 다시 요청
+    //   }
+    // }
+    return response.body;
+  }
+
+  Future<Map<String, dynamic>> postRequest(String endpoint, Map<String, dynamic> body) async {
+    // var token = await _tokenService.loadToken();
+    // if (token == null) throw Exception("No token found");
+    final url = "우리가들어갈엔드포인트/$endpoint";
+
+//
+    final springResponse = await http.post(
+      Uri.parse(url),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+       //  'accesstoken': token.accessToken
+      },
+      body: jsonEncode(body),
+    );
+
+    final SpringResponse response = SpringResponse.fromJson(jsonDecode(utf8.decode(springResponse.bodyBytes)));
+
+    // if (response.statusCode == 401) {
+    //   bool refreshed = await _authController.handle401();
+    //   if (refreshed) {
+    //     return postRequest(endpoint, body); // 다시 요청
+    //   }
+    // }
+    return response.body;
+  }
+}
